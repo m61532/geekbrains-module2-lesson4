5a690394108365365b62da0d3a4f0f9a11c22536
Mikhail <km61532@gmail.com>
2021-11-05 19:07:29 +0300

    Task has been done.

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,27 @@
 package sample;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.scene.input.KeyEvent;
+
 public class Controller {
+    public TextField chatTextField;
+    public TextArea chatTextArea;
+
+    public void sendMessage(ActionEvent actionEvent) {
+        Object source = (Button) actionEvent.getSource();
+        final String message = chatTextField.getText();
+        chatTextField.clear();
+        chatTextArea.appendText("You: " + message + "\n");
+    }
+
+    public void sendMessageByEnter(KeyEvent keyEvent) {
+        if (keyEvent.getCode().toString().equalsIgnoreCase("ENTER")) {
+            final String message = chatTextField.getText();
+            chatTextField.clear();
+            chatTextArea.appendText("You: " + message + "\n");
+        }
+    }
 }
